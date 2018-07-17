69c845f56f4baccecbfce427284b6b348f0580c9
JFerrer-IES <41206147+JFerrer-IES@users.noreply.github.com>
2018-07-17 13:49:52 -0500

    Delete UsuarioRepository.java

--- appPayU/src/main/java/com/vintec/appPayU/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vintec.appPayU;
-
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario>{
-
-
-}
